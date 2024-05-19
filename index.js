8dd7141454d1dcd1a07af3df2316c425f5aabb97
Alolii <135501680+Alolii@users.noreply.github.com>
2024-05-18 21:41:49 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+const triggerArea = document.getElementById('triggerArea');
+
+triggerArea.addEventListener('mouseover', () => {
+    const intervalId = setInterval(createCorazon, 200);
+
+    setTimeout(() => {
+        clearInterval(intervalId);
+    }, 3000);
+});
+
+function createCorazon() {
+    const corazon = document.createElement('div');
+    corazon.classList.add('corazon');
+
+    // Obtener el ancho y alto del viewport
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+
+    // Calcular coordenadas aleatorias dentro del viewport
+    const x = Math.random() * viewportWidth;
+    const y = Math.random() * viewportHeight;
+
+    corazon.style.left = `${x}px`;
+    corazon.style.top = `${y}px`;
+
+    document.body.appendChild(corazon);
+
+    setTimeout(() => {
+        corazon.remove();
+    }, 1000);
+}
